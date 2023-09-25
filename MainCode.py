79a9dd0941912c95ed6aa09f0d300bf517ff300b
Amith A <amith.hathwar02@gmail.com>
2023-09-25 19:26:23 +0530

    Done

--- MainCode.py
+++ MainCode.py
@@ -43,3 +43,48 @@ def faceBox(faceNet,frame):
 
     # Return frame with bounding boxes and list of bounding boxes        
     return frame, bboxs
+
+ageProto = 'age_deploy.prototxt'
+ageModel = 'age_net.caffemodel'
+
+genderProto = 'gender_deploy.prototxt'
+genderModel = 'gender_net.caffemodel'
+
+faceNet = cv2.dnn.readNet(faceModel,faceProto)
+ageNet = cv2.dnn.readNet(ageModel,ageProto)
+genderNet = cv2.dnn.readNet(genderModel,genderProto)
+age_list = ['(0-2)','(4-6)', '(8-12)', '(15-20)','(21-32)','(38-43)','(48-53)','(60-100)']
+gender_list = ['Male', 'Female']
+model_mean_value = (78.4263377603,87.7689143744, 114.895847746)
+video  = cv2.VideoCapture(0)
+padding = 20
+    
+while True:
+    ret,frame= video.read()
+    frame,bboxs = faceBox(faceNet,frame)
+    for bbox in bboxs:
+        face= frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        blob = cv2.dnn.blobFromImage(face,1.0 , (227,227),model_mean_value,swapRB = False)
+        
+        
+        genderNet.setInput(blob)
+        gender_pred = genderNet.forward()
+        gender = gender_list[gender_pred[0].argmax()]
+        
+        
+        ageNet.setInput(blob)
+        age_pred = ageNet.forward()
+        age = age_list[age_pred[0].argmax()]
+        
+        label = "{},{}".format(gender,age)
+        cv2.rectangle(frame, (bbox[0], bbox[1]-10), (bbox[2], bbox[1]),(0,255,0),-1)
+        cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2, cv2.LINE_AA)
+        
+        
+    cv2.imshow('ProjectGurukul Age-Gender', frame)
+    k = cv2.waitKey(1)
+    if k==ord('x'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
